4d477e00f1bb4453b8cf6df0c009b233b30ba38c
michaeldgarber <mdg711@gmail.com>
2025-11-20 09:25:20 -0700

    closer to final

--- extract-summarize-fig4a-nankivell.r
+++ extract-summarize-fig4a-nankivell.r
@@ -6,6 +6,7 @@
 
 #I measured the inches using powerpoint.
 
+# Load data-----
 #Load data and then summarize by 6-month intervals
 
 library(tidyverse)
@@ -21,23 +22,41 @@ mvi_by_dsa_by_time_bin = read_csv("iMVI-incidence-by-dsa-fig4a-nankivell-csv-for
     )
   )
 
+# Overall numbers-----
 #what do these add up to? It should be 202.
 #If not exactly, that's okay. The ratio is what matters
-mvi_by_dsa_by_time_bin %>% 
+totals_over_follow_up=mvi_by_dsa_by_time_bin %>% 
   mutate(total=1) %>% 
   group_by(total) %>% 
-  summarise(total_mvi_gt2=sum(total_mvi_gt2))
+  summarise(
+    #use the _tot subscript to differentiate from the below
+    #stratum-specific totals  
+    n_tot_mvi_gt2=sum(total_mvi_gt2),
+    n_tot_dsa_neg_blue=sum(dsa_neg_blue),
+    n_tot_dsa_pos_red=sum(dsa_pos_red)) %>% 
+  ungroup() %>% 
+  mutate(
+    #proportion DSA-negative of total
+    #Also available from Figure 1
+    prop_tot_dsa_neg=n_tot_dsa_neg_blue/n_tot_mvi_gt2,
+    prop_tot_imvi=prop_tot_dsa_neg*(50/70),
+  ) %>% 
+  dplyr::select(-total)
+
+totals_over_follow_up
 
 #okay, 195. almost
+
+#what's the overall ratio of dsa-neg to dsa-pos?
 
 
 #sum number of cases by the six-month
 mvi_by_dsa_by_six_mos=mvi_by_dsa_by_time_bin %>% 
   group_by(before_six_mos) %>% 
   summarise(
-    total_mvi_gt2=sum(total_mvi_gt2,na.rm=T),
-    dsa_pos_red=sum(dsa_pos_red,na.rm=T),
-    dsa_neg_blue=sum(dsa_neg_blue,na.rm=T)
+    n_mvi_gt2=sum(total_mvi_gt2,na.rm=T),
+    n_dsa_pos_red=sum(dsa_pos_red,na.rm=T),
+    n_dsa_neg_blue=sum(dsa_neg_blue,na.rm=T)
   ) %>% 
   #now calculate the proportion DSA-negative before and after six months
   ungroup() %>% 
@@ -46,25 +65,48 @@ mvi_by_dsa_by_six_mos=mvi_by_dsa_by_time_bin %>%
   #for calculations below
   arrange(desc(before_six_mos)) %>% 
   mutate(
-    dsa_neg_prop=dsa_neg_blue/total_mvi_gt2,
+    prop_dsa_neg=n_dsa_neg_blue/n_mvi_gt2,
     
     #From Figure 1, of the 70 total DSA-neg, 50 are C4d-negative,
     #which is the official dx of iMVI.
     #So we could assume a constant proportion of 50/70 as c4d-negative
     #though it doesn't matter because the relative share of DSA-neg before
     #and after 6 months is the critical piece. if c4d-negative share is the same,
-    #it won't change the ratio
-    imvi_prop=dsa_neg_prop*(50/70),
+    #it won't change the ratio.
+    #This is also an assumption and may not hold
+    prop_imvi=prop_dsa_neg*(50/70),
     
     #now, let's calculate the relative ratio of dsa-negative before six months
     #compared with after
     #Look down the dataset using lead
-    ratio_dsa_neg_before_six_mos=dsa_neg_prop/lead(dsa_neg_prop)
+    ratio_dsa_neg_before_six_mos=prop_dsa_neg/lead(prop_dsa_neg)
+  ) %>% 
+  #now, append the overall totals over follow-up
+  #and calculate relatively how much higher or lower the dsa-negative share
+  #is before and after six months
+  bind_cols(totals_over_follow_up) %>% 
+  mutate(
+    ratio_dsa_neg_vs_total=prop_dsa_neg/prop_tot_dsa_neg,
+    #should be the same, but check
+    ratio_imvi_vs_total=prop_imvi/prop_tot_imvi
   )
   
+  
 
-#So the answer is that iMVI is 2.285932 as likely before six months
-#as after
+mvi_by_dsa_by_six_mos %>% View()
+#So the answers:
+mvi_by_dsa_by_six_mos %>% 
+  filter(before_six_mos==1) %>% 
+  dplyr::select(ratio_dsa_neg_vs_total) %>% 
+  pull()
+
+#Before 6 months, multiply the ratio from Sablik by 1.4
+#After Sablik, multiply the ratio from Sablik by 0.6178514
+mvi_by_dsa_by_six_mos %>% 
+  filter(before_six_mos==0) %>% 
+  dplyr::select(ratio_dsa_neg_vs_total) %>% 
+  pull()
+
 
 #write this to a CSV for easier pasting into Word.
 write.csv(mvi_by_dsa_by_six_mos,"mvi_by_dsa_by_six_mos.csv")
